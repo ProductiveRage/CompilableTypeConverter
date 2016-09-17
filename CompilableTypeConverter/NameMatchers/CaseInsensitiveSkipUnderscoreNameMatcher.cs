c19066c8193ae78ac54ed3b422da7741d868c57b
ProductiveRage <742348+ProductiveRage@users.noreply.github.com>
2016-09-17 17:41:33 +0100

    Corrected comment in the standard name matcher (it said that it only ignored case but it also ignores any underscores in names)

--- CompilableTypeConverter/NameMatchers/CaseInsensitiveSkipUnderscoreNameMatcher.cs
+++ CompilableTypeConverter/NameMatchers/CaseInsensitiveSkipUnderscoreNameMatcher.cs
@@ -2,21 +2,21 @@
 
 namespace CompilableTypeConverter.NameMatchers
 {
-    /// <summary>
-    /// Determine whether two property names can be considered equal for mappings (this ignores case by allows no other transformations)
-    /// </summary>
-    public class CaseInsensitiveSkipUnderscoreNameMatcher : INameMatcher
-    {
-        public bool IsMatch(string from, string to)
-        {
-            from = (from ?? "").Trim();
-            if (from == "")
-                throw new ArgumentNullException("from");
-            to = (to ?? "").Trim();
-            if (to == "")
-                throw new ArgumentNullException("to");
+	/// <summary>
+	/// Determine whether two property names can be considered equal for mappings (this ignores case and ignores any underscore but considers no other transformations)
+	/// </summary>
+	public class CaseInsensitiveSkipUnderscoreNameMatcher : INameMatcher
+	{
+		public bool IsMatch(string from, string to)
+		{
+			from = (from ?? "").Trim();
+			if (from == "")
+				throw new ArgumentNullException("from");
+			to = (to ?? "").Trim();
+			if (to == "")
+				throw new ArgumentNullException("to");
 
-            return from.Replace("_", "").Equals(to.Replace("_", ""), StringComparison.InvariantCultureIgnoreCase);
-        }
-    }
+			return from.Replace("_", "").Equals(to.Replace("_", ""), StringComparison.OrdinalIgnoreCase);
+		}
+	}
 }
